ef46131b918cac5be10fbc4d64bdfb6e51f77259
LZS911 <932177767@qq.com>
2025-02-07 14:06:04 +0800

    [feature]: show audit execution error message

--- webapp/packages/plugin-data-viewer/src/TableViewer/TableError.tsx
+++ webapp/packages/plugin-data-viewer/src/TableViewer/TableError.tsx
@@ -65,6 +65,11 @@ const style = css`
   error-message {
     white-space: pre-wrap;
   }
+  error-sub-message {
+    display: block;
+    font-size: 0.9em;
+    margin-top: 8px;
+  }
   IconOrImage {
     width: 40px;
     height: 40px;
@@ -143,7 +148,10 @@ export const TableError = observer<Props>(function TableError({ model, loading, 
     <error {...use({ animated, collapsed: !errorInfo.display, errorHidden })} className={className}>
       <error-body>
         <IconOrImage icon={icon} title={error.message} onClick={() => errorInfo.show()} />
-        <error-message>{error.message}</error-message>
+        <div>
+          <error-message>{error.message}</error-message>
+          {error.executionFailedMessage && <error-sub-message>{error.executionFailedMessage}</error-sub-message>}
+        </div>
       </error-body>
       <controls>
         <Button type="button" mod={['outlined']} onClick={() => errorInfo.hide()}>
