a3883504b55d0631242c9574f9c7a85e5bb1c6fa
LZS911 <932177767@qq.com>
2025-02-08 13:32:53 +0800

    [chore]: add console log to print error

--- webapp/packages/core-blocks/src/useErrorDetails.tsx
+++ webapp/packages/core-blocks/src/useErrorDetails.tsx
@@ -52,6 +52,7 @@ export function useErrorDetails(error: Error | null): HookType {
   const name = error?.name;
   const message = error?.message;
   const executionFailedMessage = (error as any)?.execution_failed_message as string;
+  console.log(error, executionFailedMessage, 'print error');
 
   return {
     name,
