02be6e5ff84da72a34d41c6af676c1d42589b41a
LZS911 <932177767@qq.com>
2025-07-11 16:56:31 +0800

    add console.log

--- webapp/packages/plugin-data-viewer/src/TableViewer/TableError.tsx
+++ webapp/packages/plugin-data-viewer/src/TableViewer/TableError.tsx
@@ -215,12 +215,10 @@ export const TableError = observer<Props>(function TableError({ model, loading, 
       errorInfo.error = model.source.error || null;
       errorInfo.display = !!model.source.error;
     }
-
-    if (error.message) {
-      const isSqlContextError = error.errorCode === SQL_CONTEXT_ERROR_CODE || /SQL context .* not found/i.test(error.message);
-      if (model.source.error && isSqlContextError) {
-        handleReopenEditor();
-      }
+    console.log(error, model.source.error);
+    const isSqlContextError = error.errorCode === SQL_CONTEXT_ERROR_CODE || /SQL context .* not found/i.test(error.message);
+    if (isSqlContextError) {
+      handleReopenEditor();
     }
   }, [error.message, handleReopenEditor, model.source.error, errorInfo]);
 
