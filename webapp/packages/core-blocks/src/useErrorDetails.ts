26e23188167fade87babab391ac75c27f1bb6a56
LZS911 <932177767@qq.com>
2025-12-04 16:16:35 +0800

    fix: update field name

--- webapp/packages/core-blocks/src/useErrorDetails.ts
+++ webapp/packages/core-blocks/src/useErrorDetails.ts
@@ -55,7 +55,7 @@ export function useErrorDetails(error: IErrorDetailsHook['error']): HookType {
   };
   const name = typeof error === 'string' ? translate('core_blocks_exception_message_error_message') : error?.name;
   const message = typeof error === 'string' ? error : error?.message;
-  const executionFailedMessage = (error as any)?.execution_failed_message as string;
+  const executionFailedMessage = (error as any)?.executionFailedMessage as string;
   const errorCode = (error as any)?.errorCode as string;
 
   return {
