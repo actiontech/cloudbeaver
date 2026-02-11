1399b534d8d90546cda86196b5666541abe6e127
BugsGuru <lwqlwq888@gmail.com>
2026-02-11 15:59:25 +0800

    feat: add support for GaussDB JDBC driver
    - Updated clone-build-deps.sh to apply patches for GaussDB driver.
    - Created patch-dbeaver-gaussdb.sh to insert GaussDB driver definition into DBeaver's plugin.xml.
    - Modified plugin.xml to include GaussDB driver resources and configurations.
    - Added pom.xml for GaussDB driver to manage dependencies and build process.

--- deploy/scripts/clone-build-deps.sh
+++ deploy/scripts/clone-build-deps.sh
@@ -20,7 +20,8 @@ echo "DBeaver branch: $DBEAVER_BRANCH"
 [ ! -d dbeaver-common ] && git clone --depth 1 -b "$DBEAVER_BRANCH" https://github.com/dbeaver/dbeaver-common.git
 [ ! -d dbeaver-jdbc-libsql ] && git clone --depth 1 -b "$DBEAVER_BRANCH" https://github.com/dbeaver/dbeaver-jdbc-libsql.git
 
-# 对 dbeaver 打达梦驱动补丁
+# 对 dbeaver 打达梦、GaussDB 驱动补丁
 "$SCRIPT_DIR/patch-dbeaver-dameng.sh" "$(pwd)/dbeaver"
+"$SCRIPT_DIR/patch-dbeaver-gaussdb.sh" "$(pwd)/dbeaver"
 
 echo "Clone and patch done. You can run build from cloudbeaver/deploy (e.g. ./build-backend.sh)."
